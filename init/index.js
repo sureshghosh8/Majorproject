1267830f86a6d705144b842cef3ae30a20ec3678
suresh <8SURESHGHOSH@GMAIL.COM>
2024-05-20 21:19:04 +0530

    adding-some things

--- init/index.js
+++ init/index.js
@@ -1,20 +1,51 @@
 const mongoose=require("mongoose");
 
-const intdata=require("./data.js")
+const initData=require("./data.js")
 const Listing=require("../models/listing.js");
 
 const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust";
 
-main().then(()=>{
+main()
+.then(()=>{
     console.log("connected to DB");
 })
-.catch((err)=>console.log(err)
+.catch((err)=>{
+  console.log(err);
 
-)
+})
 
 async function main() {
     await mongoose.connect(MONGO_URL);
   }
   const intiDB=async()=>{
-Listing.deleteMany({});
-  }
+ await Listing.deleteMany({});
+ await Listing.insertMany(initData.data);
+ console.log("data was initialized");
+
+  };
+  intiDB();
+// const mongoose = require("mongoose");
+// const initData = require("./data.js");
+// const Listing = require("../models/listing.js");
+
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+// main()
+//   .then(() => {
+//     console.log("connected to DB");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// async function main() {
+//   await mongoose.connect(MONGO_URL);
+// }
+
+// const initDB = async () => {
+//   await Listing.deleteMany({});
+//   await Listing.insertMany(initData.data);
+//   console.log("data was initialized");
+// };
+
+// initDB();
--- models/listing.js
+++ models/listing.js
@@ -1,18 +1,43 @@
-const mongoose=require("mongoose");
-const Schema=mongoose.Schema;
-const listingSchema= new Schema({
-    title:{
-        type:String,
-        required:true,},
-    description:String,
-    image:{
-        type:String,
-        default:"https://images.unsplash.com/photo-1714312634720-ae1f906d09d6?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    set:(v)=>v===""?"https://images.unsplash.com/photo-1714312634720-ae1f906d09d6?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v,
-    },
-    price:Number,
-    location:String,
-    country:String,
+// const mongoose=require("mongoose");
+// const Schema=mongoose.Schema;
+// const listingSchema= new Schema({
+//     title:{
+//         type:String,
+//         required:true,},
+//     description:String,
+//     image:{
+//         type:String,
+//         default:"https://images.unsplash.com/photo-1714312634720-ae1f906d09d6?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     set:(v)=>v===""?"https://images.unsplash.com/photo-1714312634720-ae1f906d09d6?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v,
+//     },
+//     price:Number,
+//     location:String,
+//     country:String,
+// });
+// const Listing=mongoose.model("Listing",listingSchema);
+// module.exports=Listing;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
-const Listing=mongoose.model("Listing",listingSchema);
-module.exports=Listing;
+
+const Listing = mongoose.model('Listing', listingSchema);
+module.exports = Listing;
